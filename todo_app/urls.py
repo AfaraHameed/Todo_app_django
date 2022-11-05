2ae6c0c802b58ffdec4cb25bc44d0dee322b1241
AfaraHameed <afarasidheek@gmail.com>
2022-11-05 08:17:32 +0530

    style and delete task

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('',views.task,name='task'),
-    path('result',views.task_view,name='task_view')
+    path('',views.task_view,name='task_view'),
+    path('delete/<int:task_id>',views.delete,name='delete')
 
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render, redirect
 
 from todo_app.models import Task
 
 
 # Create your views here.
-def task(request):
-    if request.method=='POST':
-        name = request.POST.get('name')
-        priority = request.POST.get('priority')
-        obj = Task(name = name , priority = priority)
-        obj.save()
-    return render(request,'task.html')
+# def task(request):
+#     if request.method=='POST':
+#         name = request.POST.get('name')
+#         priority = request.POST.get('priority')
+#         obj = Task(name = name , priority = priority)
+#         obj.save()
+#     return render(request,'task.html')
 
 def task_view(request):
-    return render(request,'task_view.html')
+    obj1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        priority = request.POST.get('priority')
+        obj = Task(name=name, priority=priority)
+        obj.save()
+    return render(request,'task_view.html',{'tasks':obj1})
+def delete(request,task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method=='POST':
+        task.delete()
+        return redirect('/')
+    return  render(request,'delete.html',{'task':task})
